2b2512cc45331e52692cec2afe976967e12f1e43
msipod3394 <82072035+msipod3394@users.noreply.github.com>
2021-04-14 16:45:41 +0900

    Extract CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,42 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPugin = require('html-webpack-plugin');
 
 module.exports = {
+
+    // モード
+    // development | production |none 
+    mode: 'development',
+
     entry: './src/script/index.js',
+
+    //  出力ディレクトリ
     output: {
-        path: path.resolve(__dirname, './dist/assets/js'),
+        path: path.resolve(__dirname, './dist'),
         // filename: 'index.js'
     },
+
     module :{
         rules: [
             {
                 test: /\.css/,
                 use: [
                     {
-                        loader: 'style-loader'
+                        loader: MiniCssExtractPlugin.loader, // javascriptとしてバンドルせず css として出力する
                     },
                     {
-                        loader: 'css-loader'
+                        loader: 'css-loader' //SASSをCSSに変換
                     }
                 ]
             }
         ]
-    }
-}
+    },
 
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPugin({
+            template: './src/index.html',
+        }),
+    ],
+
+}
